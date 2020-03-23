cdb92dd8cc3aba9407fa2fede6928511a8743a7d
Gregorio Marciano <gregoriomarciano@gmail.com>
2020-03-23 11:36:19 -0600

    migracion

--- code/Confluence2AzureDevOps/Confluence2AzureDevOpsTests/TestForWikiMigrator.cs
+++ code/Confluence2AzureDevOps/Confluence2AzureDevOpsTests/TestForWikiMigrator.cs
@@ -1,3 +1,4 @@
+using System;
 using Confluence2AzureDevOps;
 using Confluence2AzureDevOps.ObjectModel;
 using Confluence2AzureDevOpsTests.UtilsForTesting;
@@ -16,19 +17,19 @@ namespace Confluence2AzureDevOpsTests
             MigrationConfig config = TestUtils.GetMigrationConfigTest();
             
             _wikiMigrator = new WikiMigrator(config);
-            
-            _wikiMigrator.StartMigration();
-            
-            // Init test values.
-            //LocalSettingTests setting = TestUtils.GetLocalSettings();
-
-//            _project = TestContext.Parameters["project"];
-//            
-//            _wikiIdentifier = TestContext.Parameters["wikiIdentifier"];
-//            
-//            _personalAccesToken = TestContext.Parameters["personalAccesToken"];
-
-//            _target = new PagesApi(_organization, _project, _wikiIdentifier, _personalAccesToken);
+        }
+        
+        [Test]
+        public void StartMigrationTest()
+        {
+            try
+            {    
+                _wikiMigrator.StartMigration();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message + "" + e.StackTrace);
+            }
         }
     }
 }
